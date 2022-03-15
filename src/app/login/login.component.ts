96f2a0a30bd41803fbb0ca2ff9d320276ba272a0
xblyed <xblyed@protonmail.com>
2022-03-15 10:14:12 +0100

    hardcoded login user and pass, error msg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ingresar() {
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if (usuario == 'admin' && password == 'admin123') {
+      // dashboard
+      console.log('Correcto');
+    } else {
+      this.showError();
+    }
+  }
+
+  showError() {
+    this._snackBar.open('Usuario o contraseña incorrectos', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+
+  ngOnInit(): void {}
 }
