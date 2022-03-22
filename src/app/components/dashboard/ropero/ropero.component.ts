3618c1fd886a5ba5233d178c4194161fe7418eaf
xblyed <xblyed@protonmail.com>
2022-03-22 19:30:28 +0100

    added action icons

--- src/app/components/dashboard/ropero/ropero.component.ts
+++ src/app/components/dashboard/ropero/ropero.component.ts
@@ -7,7 +7,7 @@ export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
-  symbol: string;
+  acciones: string;
 }
 
 @Component({
@@ -16,30 +16,36 @@ export interface PeriodicElement {
   styleUrls: ['./ropero.component.css'],
 })
 export class RoperoComponent implements AfterViewInit {
+  // creamos una variable de elementos vacía
   ELEMENT_DATA: PeriodicElement[] = [];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'acciones'];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  // para el paginator, cosas de material
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
+  // para el filtro, cosas de material
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  // inyectamos el servicio
   constructor(private _itemsService: ItemsService) {}
 
-  ngOnInit(): void {
-    this.cargarItems();
-  }
-
+  //creamos una funcion para cargar los datos desde el servicio
   cargarItems() {
     this.ELEMENT_DATA = this._itemsService.getItems();
     this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  }
+
+  // ejecutamos la funcion al inicio
+  ngOnInit(): void {
+    this.cargarItems();
   }
 }
