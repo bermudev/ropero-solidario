f588fc09bd503d81bdd0d4144c47fd7ddedc4cd2
bermudev <carlos.bdez.esc@gmail.com>
2022-04-03 00:40:07 +0200

    added data type, added new item, improved UI

--- src/app/components/dashboard/add-item/add-item.component.ts
+++ src/app/components/dashboard/add-item/add-item.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Item } from 'src/app/interfaces/item';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
-  styleUrls: ['./add-item.component.css']
+  styleUrls: ['./add-item.component.css'],
 })
 export class AddItemComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _itemService: ItemsService) {
+    this.form = this.fb.group({
+      nombre: ["", Validators.required],
+      categoria: ["", Validators.required],
+      cantidad: ["", Validators.required]
 
-  ngOnInit(): void {
+    })
   }
 
+  ngOnInit(): void {}
+
+  agregarItem() {
+    const item: Item = {
+      id: 0,
+      nombre: this.form.value.nombre,
+      categoria: this.form.value.categoria,
+      cantidad: this.form.value.cantidad
+    }
+
+    console.log(item);
+    this._itemService.agregarItem(item)
+    
+  }
 }
+
+
--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,31 +1,15 @@
 import { Injectable } from '@angular/core';
 import { PeriodicElement } from '../components/dashboard/ropero/ropero.component';
+import { Item } from '../interfaces/item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemsService {
   ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, acciones: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, acciones: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, acciones: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, acciones: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, acciones: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, acciones: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, acciones: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, acciones: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, acciones: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, acciones: 'Ne' },
-    { position: 11, name: 'Sodium', weight: 22.9897, acciones: 'Na' },
-    { position: 12, name: 'Magnesium', weight: 24.305, acciones: 'Mg' },
-    { position: 13, name: 'Aluminum', weight: 26.9815, acciones: 'Al' },
-    { position: 14, name: 'Silicon', weight: 28.0855, acciones: 'Si' },
-    { position: 15, name: 'Phosphorus', weight: 30.9738, acciones: 'P' },
-    { position: 16, name: 'Sulfur', weight: 32.065, acciones: 'S' },
-    { position: 17, name: 'Chlorine', weight: 35.453, acciones: 'Cl' },
-    { position: 18, name: 'Argon', weight: 39.948, acciones: 'Ar' },
-    { position: 19, name: 'Potassium', weight: 39.0983, acciones: 'K' },
-    { position: 20, name: 'Calcium', weight: 40.078, acciones: 'Ca' },
+    { id: 1, nombre: 'Chaleco', categoria:"Invierno", cantidad: 2},
+    { id: 2, nombre: 'Pantalón', categoria:"Primavera", cantidad: 4},
+    { id: 3, nombre: 'Camiseta', categoria:"Verano", cantidad: 6},
   ];
 
   constructor() {}
@@ -40,10 +24,15 @@ export class ItemsService {
   // https://bobbyhadz.com/blog/javascript-remove-object-from-array-by-value
   eliminarItem(index: number){
     const indexOfObject = this.ELEMENT_DATA.findIndex(object => {
-      return object.position === index;
+      return object.id === index;
     })
 
     //aqui realmente se haría el query http
     this.ELEMENT_DATA.splice(indexOfObject, 1)
+  }
+
+  agregarItem(item: Item){
+    item.id = this.ELEMENT_DATA.length + 1
+    this.ELEMENT_DATA.unshift(item)
   }
 }
