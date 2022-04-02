c1acee938b1df79554dc131921f24bbe08f24289
xbLyed <xblyed@protonmail.com>
2022-04-02 22:57:35 +0000

    Revert "added form reset and snackbar"
    This reverts commit 9e609edc10386fcb9c95fed3ecb76cfbbc4bdbcd

--- src/app/components/dashboard/add-item/add-item.component.ts
+++ src/app/components/dashboard/add-item/add-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Item } from 'src/app/interfaces/item';
 import { ItemsService } from 'src/app/services/items.service';
 
@@ -12,16 +11,13 @@ import { ItemsService } from 'src/app/services/items.service';
 export class AddItemComponent implements OnInit {
   form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private _itemService: ItemsService,
-    private _snackBar: MatSnackBar
-  ) {
+  constructor(private fb: FormBuilder, private _itemService: ItemsService) {
     this.form = this.fb.group({
-      nombre: ['', Validators.required],
-      categoria: ['', Validators.required],
-      cantidad: ['', Validators.required],
-    });
+      nombre: ["", Validators.required],
+      categoria: ["", Validators.required],
+      cantidad: ["", Validators.required]
+
+    })
   }
 
   ngOnInit(): void {}
@@ -31,19 +27,13 @@ export class AddItemComponent implements OnInit {
       id: 0,
       nombre: this.form.value.nombre,
       categoria: this.form.value.categoria,
-      cantidad: this.form.value.cantidad,
-    };
-    this._itemService.agregarItem(item);
+      cantidad: this.form.value.cantidad
+    }
 
-    this.form.reset();
-    this.showSnack();
-  }
-
-  showSnack() {
-    this._snackBar.open('Objeto agregado correctamente', '', {
-      duration: 5000,
-      horizontalPosition: 'center',
-      verticalPosition: 'bottom',
-    });
+    console.log(item);
+    this._itemService.agregarItem(item)
+    
   }
 }
+
+
