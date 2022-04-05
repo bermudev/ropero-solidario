03f1d5ea00f9be4f3f1ec635dd03b3e4813c4742
xblyed <xblyed@protonmail.com>
2022-04-05 21:11:42 +0200

    added snackbar after item deletion

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ObjectsList } from '../components/dashboard/ropero/ropero.component';
 import { Item } from '../interfaces/item';
 
@@ -12,7 +13,7 @@ export class ItemsService {
     { id: 3, nombre: 'Camiseta', categoria: 'Verano', cantidad: 6 },
   ];
 
-  constructor() {}
+  constructor(private _snackBar: MatSnackBar) {}
 
   // devolvemos en los otros ts el elemento con los datos
   getItems() {
@@ -24,16 +25,25 @@ export class ItemsService {
   // https://bobbyhadz.com/blog/javascript-remove-object-from-array-by-value
   eliminarItem(index: number) {
     const indexOfObject = this.ELEMENT_DATA.findIndex((object) => {
-      console.log(object.id);
       return object.id === index;
     });
 
     //aqui realmente se haría el query http
     this.ELEMENT_DATA.splice(indexOfObject, 1);
+    this.showSnack()
+    
   }
 
   agregarItem(item: Item) {
     item.id = this.ELEMENT_DATA.length + 1;
     this.ELEMENT_DATA.unshift(item);
+  }
+
+  showSnack() {
+    this._snackBar.open('Objeto eliminado correctamente', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
   }
 }
