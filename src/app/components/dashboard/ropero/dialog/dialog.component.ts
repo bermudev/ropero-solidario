0e85a71048a4685c4a38d38151183ed8444a6a16
xblyed <xblyed@protonmail.com>
2022-04-05 20:47:09 +0200

    changed dialog to angular material

---
+++ src/app/components/dashboard/ropero/dialog/dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css'],
+})
+export class DialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+
+    // en la siguiente linea le pasamos el data de ropero.component.ts
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  
+
+  ngOnInit(): void {}
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+  confirm() {
+  }
+}
