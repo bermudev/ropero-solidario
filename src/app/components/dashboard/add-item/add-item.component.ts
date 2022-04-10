b42611190ddf94bafc72cda0af39ff08ef8eee04
bermudev <carlos.bdez.esc@gmail.com>
2022-04-10 16:02:40 +0200

    added edit functionality, modified add function to serve as both add and edit

--- src/app/components/dashboard/add-item/add-item.component.ts
+++ src/app/components/dashboard/add-item/add-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Item } from 'src/app/interfaces/item';
 import { ItemsService } from 'src/app/services/items.service';
 
@@ -11,20 +12,34 @@ import { ItemsService } from 'src/app/services/items.service';
 })
 export class AddItemComponent implements OnInit {
   form: FormGroup;
+  idItem: any;
+  action = 'Agregar';
 
   constructor(
     private fb: FormBuilder,
     private _itemService: ItemsService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private aRoute: ActivatedRoute
   ) {
     this.form = this.fb.group({
       nombre: ['', Validators.required],
       categoria: ['', Validators.required],
       cantidad: ['', Validators.required],
     });
+
+    // esto es un poco de magia
+    // si es crear idItem es undefined y si es editar es un numero
+    const idParam = 'id';
+    this.idItem = this.aRoute.snapshot.params[idParam];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.idItem !== undefined) {
+      this.action = 'Editar';
+      this.esEditar();
+    }
+  }
 
   agregarItem() {
     const item: Item = {
@@ -33,17 +48,38 @@ export class AddItemComponent implements OnInit {
       categoria: this.form.value.categoria,
       cantidad: this.form.value.cantidad,
     };
-    this._itemService.agregarItem(item);
 
-    this.form.reset();
-    this.showSnack();
+    // preguntamos si es agregar o editar
+    if (this.idItem !== undefined) {
+      this.editItem(item);
+    } else {
+      this.addItem(item);
+    }
   }
 
-  showSnack() {
+  editItem(item: Item) {
+    this._itemService.editarItem(item, item.id);
+    this._snackBar.open('Objeto editado correctamente', '', {
+      duration: 5000,
+    });
+    this.router.navigate(['/dashboard']);
+  }
+
+  addItem(item: Item) {
+    this._itemService.agregarItem(item);
     this._snackBar.open('Objeto agregado correctamente', '', {
       duration: 5000,
-      horizontalPosition: 'center',
-      verticalPosition: 'bottom',
+    });
+    this.router.navigate(['/dashboard']);
+  }
+
+  esEditar() {
+    const item: Item = this._itemService.getSingleItem(this.idItem);
+    console.log(item);
+    this.form.patchValue({
+      nombre: item.nombre,
+      categoria: item.categoria,
+      cantidad: item.cantidad,
     });
   }
 }
