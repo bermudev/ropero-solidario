f9a5418c669233f7e2d50196d344c2d6fe8ef1ef
bermudev <carlos.bdez.esc@gmail.com>
2022-04-22 08:47:00 +0200

    added api to category screen, everything working correctly

--- src/app/components/dashboard/categorias/categorias.component.ts
+++ src/app/components/dashboard/categorias/categorias.component.ts
@@ -11,16 +11,20 @@ import { AuthService } from 'src/app/services/auth.service';
 export class CategoriasComponent implements OnInit {
   data: Category[] = [];
   names: string[] = [];
+  isMobile = false;
 
-  selectedAddCategory: string = ""
-  selectedDelCategory: string = ""
-  selectedEditCategory: string = ""
-  selectedEditNewCategory: string = ""
+  selectedAddCategory: string = '';
+  selectedDelCategory: string = '';
+  selectedEditCategory: string = '';
+  selectedEditNewCategory: string = '';
 
-  constructor(private authService: AuthService) {
-  }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.loadFromAPI();
+  }
+
+  loadFromAPI() {
     this.authService.getCategories().subscribe((data) => {
       // hacemos que el json pase a string
       this.data = JSON.parse(JSON.stringify(data));
@@ -36,7 +40,6 @@ export class CategoriasComponent implements OnInit {
     });
   }
 
-  isMobile = false;
   getIsMobile(): boolean {
     const w = document.documentElement.clientWidth;
     const breakpoint = 991;
@@ -49,15 +52,43 @@ export class CategoriasComponent implements OnInit {
   }
 
   addCategoryClick() {
-    console.log(`Agregar categoria ${this.selectedAddCategory}`);
+    //console.log(`Agregar categoria ${this.selectedAddCategory}`);
+
+    const category: Category = {
+      name: this.selectedAddCategory,
+    };
+
+    this.authService.addCategorie(category);
+    this.loadFromAPI()
   }
 
   delCategoryClick() {
-    console.log(`Eliminar categoria ${this.selectedDelCategory}`);
+    //console.log(`Eliminar categoria ${this.selectedDelCategory}`);
+
+    const category = this.data.find((d) => d.name === this.selectedDelCategory);
+    
+    // necesito extraer el id buscando en el objeto por el nombre del data
+    // console.log(idCategory?.id);
+
+    // le pongo el ! al final para decirle que no es undefined, el signo de interrogacion la verdad que no lo se
+    this.authService.deleteCategorie(category?.id!)
+    this.loadFromAPI()
   }
 
   editCategoryClick() {
-    console.log(`Editar categoria ${this.selectedEditCategory}`);
-    console.log(`Nueva categoria ${this.selectedEditNewCategory}`);
+    //console.log(`Editar categoria ${this.selectedEditCategory}`);
+    //console.log(`Nueva categoria ${this.selectedEditNewCategory}`);
+
+    // las dos interrogaciones es para decirles que ambos va a tener typo y no seran undefined (no se si es buena practica)
+    const oldCategoryID = this.data.find((d) => d.name === this.selectedEditCategory)!.id!;
+    
+    const category: Category = {
+      name: this.selectedEditNewCategory,
+    };
+
+    // le mandamos el name de selectedEditNewCategory y el id del selectedEditCategory
+    this.authService.editCategorie(category, oldCategoryID)
+
+    this.loadFromAPI()
   }
 }
